9d86f5cc2f75bb23120500bb3b4a8d983d93d924
mani171218 <mani.kandan@reddway.in>
2021-10-19 23:27:26 +0530

    Bug fix

--- src/app/models/joinTheNational.ts
+++ src/app/models/joinTheNational.ts
@@ -2,8 +2,8 @@ export class joinTheNational{
     constructor(
         public FirstName:string,
         public LastName:string,
-        public Email:string,
-        public Phone:string,
+        public EmailId:string,
+        public PhoneNumber:string,
         public CompanyName:string
     ){}
 }
